dd219a84afcc6d89de90ca331be4142cde1f2b87
mohamed fawzy <meniashow@gmail.com>
2019-08-08 21:50:27 +0200

    "created task"

--- app/src/main/java/com/andalus/abomedhat/mohamedfawzytasks/MainActivity.java
+++ app/src/main/java/com/andalus/abomedhat/mohamedfawzytasks/MainActivity.java
@@ -2,6 +2,12 @@ package com.andalus.abomedhat.mohamedfawzytasks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<DataSet> item=new ArrayList<>();
+        item.add(new DataSet(R.drawable.ic_launcher_background,"Twitter","45 MB"));
+        item.add(new DataSet(R.drawable.ic_launcher_background,"Twitter","45 MB"));
+        item.add(new DataSet(R.drawable.ic_launcher_background,"Twitter","45 MB"));
+        item.add(new DataSet(R.drawable.ic_launcher_background,"Twitter","45 MB"));
+        item.add(new DataSet(R.drawable.ic_launcher_background,"Twitter","45 MB"));
+        item.add(new DataSet(R.drawable.ic_launcher_background,"Twitter","45 MB"));
+        item.add(new DataSet(R.drawable.ic_launcher_background,"Twitter","45 MB"));
+        item.add(new DataSet(R.drawable.ic_launcher_background,"Twitter","45 MB"));
+        item.add(new DataSet(R.drawable.ic_launcher_background,"Twitter","45 MB"));
+        item.add(new DataSet(R.drawable.ic_launcher_background,"Twitter","45 MB"));
+        item.add(new DataSet(R.drawable.ic_launcher_background,"Twitter","45 MB"));
+        item.add(new DataSet(R.drawable.ic_launcher_background,"Twitter","45 MB"));
+        item.add(new DataSet(R.drawable.ic_launcher_background,"Twitter","45 MB"));
+        Recycler recycler=new Recycler(this,item);
+        RecyclerView recyclerView=findViewById(R.id.recycler);
+        GridLayoutManager gridLayoutManager=new GridLayoutManager(this,numberPossible());
+        recyclerView.setLayoutManager(gridLayoutManager);
+        recyclerView.setAdapter(recycler);
     }
+    public int numberPossible() {
+        DisplayMetrics displayMetrics = this.getResources().getDisplayMetrics();
+        float dpwidth = displayMetrics.widthPixels / displayMetrics.density;
+        int numberPossible = (int) (dpwidth / 170);
+        return numberPossible;
+    }
+
 }
